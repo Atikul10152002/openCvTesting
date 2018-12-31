bd722ef7a0ff8d34712609933804d535e76b5f44
Mohammad Islam <Atikul10152002@gmail.com>
2018-12-30 19:27:00 -0500

    update: jupyter notebook examples

---
+++ jupyter-notebook/hsv_val.py
@@ -0,0 +1,31 @@
+# hueLow = 27
+# hueHigh = 52
+# saturationLow = 107
+# saturationHigh = 215
+# valueLow = 4
+# valueHigh = 255
+# blur = 19
+##########
+hueLow = 99
+hueHigh = 106
+saturationLow = 75
+saturationHigh = 128
+valueLow = 25
+valueHigh = 169
+blur = 21
+##########
+hueLow = 15
+hueHigh = 255
+saturationLow = 0
+saturationHigh = 255
+valueLow = 0
+valueHigh = 255
+blur = 1
+##########
+hueLow = 15
+hueHigh = 255
+saturationLow = 0
+saturationHigh = 255
+valueLow = 0
+valueHigh = 255
+blur = 1
