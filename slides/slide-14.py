06fc76089c2bf6f85de3f577b06626a42f9c26d2
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2018-12-31 16:36:09 -0500

    Save changes

---
+++ slides/slide-14.py
@@ -0,0 +1,79 @@
+import cv2
+import numpy as np
+
+# Create an instance of camera 0
+cap = cv2.VideoCapture(0)
+
+win = 'Result'
+nothing = lambda *args, **kwargs: None
+
+# create window with name win
+cv2.namedWindow(win, cv2.WINDOW_AUTOSIZE)
+
+
+# create trackbars
+hl = cv2.createTrackbar('Hue Low',  win, 0, 179, nothing)
+hh = cv2.createTrackbar('Hue High', win, 100, 179, nothing)
+sl = cv2.createTrackbar('Saturation Low',  win, 0, 255, nothing)
+sh = cv2.createTrackbar('Saturation High', win, 100, 255, nothing)
+vl = cv2.createTrackbar('Value Low',  win, 0, 255, nothing)
+vh = cv2.createTrackbar('Value High', win, 100, 255, nothing)
+
+
+while True:
+    # Get the image from camera 0
+    _, image = cap.read()
+
+    # show image under window
+    cv2.imshow("Raw Camera Data", image)
+
+    result = cv2.cvtColor(
+        image,
+        cv2.COLOR_BGR2HSV
+    )
+
+    # get values from trackbars
+    HueLow = cv2.getTrackbarPos('Hue Low',  win)
+    HueHigh = cv2.getTrackbarPos('Hue High', win)
+    SatLow = cv2.getTrackbarPos('Saturation Low',  win)
+    SatHigh = cv2.getTrackbarPos('Saturation High', win)
+    ValLow = cv2.getTrackbarPos('Value Low',  win)
+    ValHigh = cv2.getTrackbarPos('Value High', win)
+
+    # Literal values
+    HSV_LOW = np.array([HueLow, SatLow, ValLow])
+    HSV_HIGH = np.array([HueHigh, SatHigh, ValHigh])
+
+    # Filter values with mask
+    result = cv2.bitwise_and(
+        result,
+        result,
+        mask=cv2.inRange(result, HSV_LOW, HSV_HIGH)
+    )
+
+    # Convert result to BGR then to GRAY
+    result = cv2.cvtColor(
+        result,
+        cv2.COLOR_HSV2BGR
+    )
+    result = cv2.cvtColor(
+        result,
+        cv2.COLOR_BGR2GRAY
+    )
+
+    ret, result = cv2.threshold(imgray, 127, 255, 0)
+    result, contours, hierarchy = cv2.findContours(
+        result,
+        cv2.RETR_TREE,
+        cv2.CHAIN_APPROX_SIMPLE
+    )
+
+    # result image under window
+    cv2.imshow(win, result)
+
+    # press 'q' key to break
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+# stop
+cv2.destroyAllWindows()
