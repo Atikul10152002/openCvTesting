0614674755a32f510daf5d01bb5614049b518a3a
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2018-12-31 16:23:18 -0500

    Added 14th slide program

--- template/matchTemplate.py
+++ template/matchTemplate.py
@@ -1,22 +1,22 @@
 import cv2
 import numpy as np
 
-#? imread(filename[, flags]) -> retval
+# ? imread(filename[, flags]) -> retval
 # * loads the templete image
 template = cv2.imread('template.png', 0)
 w, h = template.shape[::-1]
 
-#? imread(filename[, flags]) -> retval
+# ? imread(filename[, flags]) -> retval
 # * loads the testing image
 img_rgb = cv2.imread('test_image.png')
 
-#? cvtColor(src, code[, dst[, dstCn]]) -> dst
-#* Converting testing image to grascale
+# ? cvtColor(src, code[, dst[, dstCn]]) -> dst
+# * Converting testing image to grascale
 img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-#? matchTemplate(image, templ, method[, result[, mask]]) -> result
-#* The function slides through image , compares the overlapped patches of size
-#* Fiding all sections of the image matching the templete
+# ? matchTemplate(image, templ, method[, result[, mask]]) -> result
+# * The function slides through image , compares the overlapped patches of size
+# * Fiding all sections of the image matching the templete
 res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
 
 threshold = 0.8
@@ -29,8 +29,8 @@ for point in zip(*loc[::-1]):
     cv2.rectangle(img_rgb, point,
                   (point[0] + w, point[1] + h), (0, 0, 255), 2)
 
-#? imshow(winname, mat) -> None
-#* displaying the modified testing image
+# ? imshow(winname, mat) -> None
+# * displaying the modified testing image
 cv2.imshow('Template Matching', img_rgb)
 
 # * close all window when any buttong is pressed
