8ac0dd83e0c362fdc94eff2820edb53651e1d16a
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2018-12-31 16:20:06 -0500

    Added 13th slide

--- slides/slide-13.py
+++ slides/slide-13.py
@@ -4,16 +4,20 @@ import numpy as np
 # Create an instance of camera 0
 cap = cv2.VideoCapture(0)
 
-win = 'Input'
-cv2.namedWindow(win, cv2.WINDOW_AUTOSIZE)
+win = 'Result'
 nothing = lambda *args, **kwargs: None
 
+# create window with name win
+cv2.namedWindow(win, cv2.WINDOW_AUTOSIZE)
+
+
+# create trackbars
 hl = cv2.createTrackbar('Hue Low',  win, 0, 179, nothing)
-hh = cv2.createTrackbar('Hue High', win, 0, 179, nothing)
+hh = cv2.createTrackbar('Hue High', win, 100, 179, nothing)
 sl = cv2.createTrackbar('Saturation Low',  win, 0, 255, nothing)
-sh = cv2.createTrackbar('Saturation High', win, 0, 255, nothing)
+sh = cv2.createTrackbar('Saturation High', win, 100, 255, nothing)
 vl = cv2.createTrackbar('Value Low',  win, 0, 255, nothing)
-vh = cv2.createTrackbar('Value High', win, 0, 255, nothing)
+vh = cv2.createTrackbar('Value High', win, 100, 255, nothing)
 
 
 while True:
@@ -28,6 +32,7 @@ while True:
         cv2.COLOR_BGR2HSV
     )
 
+    # get values from trackbars
     HueLow = cv2.getTrackbarPos('Hue Low',  win)
     HueHigh = cv2.getTrackbarPos('Hue High', win)
     SatLow = cv2.getTrackbarPos('Saturation Low',  win)
@@ -46,6 +51,7 @@ while True:
         mask=cv2.inRange(result, HSV_LOW, HSV_HIGH)
     )
 
+    # Convert result to BGR then to GRAY
     result = cv2.cvtColor(
         result,
         cv2.COLOR_HSV2BGR
@@ -56,7 +62,7 @@ while True:
     )
 
     # result image under window
-    cv2.imshow("Result", result)
+    cv2.imshow(win, result)
 
     # press 'q' key to break
     if cv2.waitKey(1) & 0xFF == ord("q"):
