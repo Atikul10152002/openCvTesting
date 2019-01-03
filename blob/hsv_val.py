e660f1d5415d5d0b025b48403bd09afca63b0058
Mohammad Islam <Atikul10152002@gmail.com>
2019-01-03 11:55:56 -0500

    update: trails

--- blob/hsv_val.py
+++ blob/hsv_val.py
@@ -1,11 +1,11 @@
 ##########
-hueLow = 24
-hueHigh = 58
-saturationLow = 88
-saturationHigh = 207
-valueLow = 120
-valueHigh = 255
-blur = 33
+# hueLow = 24
+# hueHigh = 58
+# saturationLow = 88
+# saturationHigh = 207
+# valueLow = 120
+# valueHigh = 255
+# blur = 33
 ########## FUEL BALL
 hueLow = 24
 hueHigh = 58
@@ -15,22 +15,6 @@ valueLow = 120
 valueHigh = 255
 blur = 33
 ########## CUBE
-hueLow = 17
-hueHigh = 44
-saturationLow = 73
-saturationHigh = 220
-valueLow = 53
-valueHigh = 170
-blur = 13
-##########
-hueLow = 18
-hueHigh = 158
-saturationLow = 66
-saturationHigh = 212
-valueLow = 86
-valueHigh = 215
-blur = 1
-##########
 hueLow = 24
 hueHigh = 33
 saturationLow = 107
